0404642025109db419d39cb3d3a8c892c242e242
Ravindu <85988392+Ravindu26881@users.noreply.github.com>
2025-08-04 00:04:28 +0530

    added more fields to products ep

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: String, required: true },
-    category: { type: String, required: true }
+    category: { type: String, required: true },
+    image: { type: String, required: false },
+    description: { type: String, required: false },
+    quantity: { type: Number, required: false },
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ංconst express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
